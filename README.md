031f1f5d40bc8a7274bb50199cb1acf314cff66f
Matthew Wohlbach <mwohlbach@Matthews-MacBook-Pro.local>
2018-04-06 09:25:48 -0400

    fixed typo

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 create - Resets current game and starts a new lobby.  
       join - Joins the game lobby.  
       start - Starts the game and sends everyone in lobby their roles.  
-      draw - President uses once Chancellor is elected. The bot will pm you 3 tiles of the top of the deck.  
+      draw - President uses once Chancellor is elected. The bot will pm you 3 tiles off the top of the deck.  
       fascist - Plays a fascist tile.  
       liberal - Plays a liberal tile.  
       players - Displays the list of players.  
--- main.py
+++ main.py
@@ -230,7 +230,7 @@ def slackEntryPoint():
             create - Resets current game and starts a new lobby.
             join - Joins the game lobby.
             start - Starts the game and sends everyone in lobby their roles.
-            draw - President uses once Chancellor is elected. The bot will pm you 3 tiles of the top of the deck.
+            draw - President uses once Chancellor is elected. The bot will pm you 3 tiles off the top of the deck.
             fascist - Plays a fascist tile.
             liberal - Plays a liberal tile.
             players - Displays the list of players.
